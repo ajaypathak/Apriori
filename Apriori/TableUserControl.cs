622a092d65c829635138d2d718dcfb43b7bb144d
Ajay Pathak <APathak2@slb.com>
2023-03-31 17:17:43 +0530

    updated the code to generate the files

--- Apriori/TableUserControl.cs
+++ Apriori/TableUserControl.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using BAL;
+using System.IO;
 
 namespace Apriori
 {
@@ -17,12 +18,32 @@ namespace Apriori
         {
             InitializeComponent();
             ItemSetLabel.Text = itemSet.Label;
+            DirectoryInfo directoryInfo = new DirectoryInfo(@"C:\Users\apathak2\source\repos\ajaypathak\Apriori\Apriori\");
+            string filename = itemSet.Label+".txt";
+            string rulefile = itemSet.Label + "rule.txt";
+            StringBuilder stringBuilder = new StringBuilder();
+            Dictionary<string, int> myDict = new Dictionary<string, int>();
+            Dictionary<string, double> rulesdict = new Dictionary<string, double>();
+
             foreach (var item in itemSet)
             {
                 ItemSetsDataGridView.Rows.Add(item.Key.ToDisplay(), item.Value);
-                if (item.Value < itemSet.Support)
+                
+                myDict.Add(item.Key.ToDisplay(), item.Value);
+                if (item.Value < itemSet.Support)                    
                     ItemSetsDataGridView.Rows[ItemSetsDataGridView.Rows.Count - 1].DefaultCellStyle.BackColor = System.Drawing.Color.LightGray;
             }
+            
+            var sortedDict = from entry in myDict orderby entry.Value descending select entry;
+            foreach (var item in sortedDict)
+            {
+                stringBuilder.Append("ItemSet:{ " + item.Key).Append("} \t Values : ").Append(item.Value).AppendLine();
+            }
+
+            filename = Path.Combine(directoryInfo.FullName, filename);
+            File.WriteAllText(filename, stringBuilder.ToString());
+            myDict.Clear();
+
             if (rules.Count == 0)
             {
                 ItemSetsDataGridView.Height = 342;
@@ -32,9 +53,25 @@ namespace Apriori
             {
                 foreach (var item in rules)
                 {
-                    RulesDataGridView.Rows.Add(item.Label, item.Confidance.ToPercentString(), item.Support.ToPercentString(),item.Description);
+                    if (rulesdict.ContainsKey(item.Label)==false)
+                    { rulesdict.Add(item.Label, item.Confidance);
+
+                    }
+                   
+
+                    RulesDataGridView.Rows.Add(item.Label, item.Confidance.ToPercentString(), 
+                        item.Support.ToPercentString(),item.Description);
                 }
             }
+            var rulesortedDict = from entry in rulesdict orderby entry.Value descending select entry;
+            stringBuilder= new StringBuilder();
+            foreach (var item in rulesortedDict)
+            {
+                stringBuilder.Append("ItemSet:{ " + item.Key).Append("} \t Confidance : ").Append(item.Value).AppendLine();
+            }
+            filename = Path.Combine(directoryInfo.FullName, rulefile);
+            File.WriteAllText(filename, stringBuilder.ToString());
+
         }
 
         public TableUserControl(List<string> Values)
